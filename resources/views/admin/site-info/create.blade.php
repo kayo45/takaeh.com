ab67f150ffc9be8b30c0bb976576fdd6b41c1a29
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-01-13 14:18:28 +0700

    Update create.blade.php

--- resources/views/admin/site-info/create.blade.php
+++ resources/views/admin/site-info/create.blade.php
@@ -224,7 +224,7 @@
 @endsection
 @push('scripts')
 <!--CKEditor JS-->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="{{asset('ckeditor/ckeditor.js')}}"></script>
 <script type="text/javascript">
     $(document).ready(function () {
