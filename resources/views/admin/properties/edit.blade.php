942fa7ddb0fdaac3be55657ca68e1b7b53218576
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-01-16 15:36:20 +0700

    Update edit.blade.php

--- resources/views/admin/properties/edit.blade.php
+++ resources/views/admin/properties/edit.blade.php
@@ -323,9 +323,21 @@
 <script src="{{asset('ckeditor/ckeditor.js')}}"></script>
 <script type="text/javascript">
     CKEDITOR.replace('content');
-    $(document).ready(function () {
-        $('.ckeditor').ckeditor();
+    CKEDITOR.on("instanceReady", function(event) {
+        event.editor.on("beforeCommandExec", function(event) {
+            // Show the paste dialog for the paste buttons and right-click paste
+            if (event.data.name == "paste") {
+                event.editor._.forcePasteDialog = true;
+            }
+            // Don't show the paste dialog for Ctrl+Shift+V
+            if (event.data.name == "pastetext" && event.data.commandData.from == "keystrokeHandler") {
+                event.cancel();
+            }
+        })
     });
+    // $(document).ready(function () {
+    //     $('.ckeditor').ckeditor();
+    // });
 </script>
 
 <script>
